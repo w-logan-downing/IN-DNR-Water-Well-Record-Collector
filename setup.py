9a5f9d317f37c914bdbc55538d603e25018c6c5e
W. Logan Downing <30451101+Foxhound013@users.noreply.github.com>
2017-11-22 15:14:52 -0600

    Files to build EXE
    Setup.py is still a work in progress but currently allows cx_Freeze to build out an exe file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+import os.path
+import numpy
+from cx_Freeze import setup, Executable
+
+add_ons = ["pdfkit", "pandas", "numpy", "numpy.core._methods", "numpy.lib.format", "tkinter", "webbrowser"]
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+setup(name = "Water Well Record Collection Tool" ,
+      version = "0.8" ,
+      description = "This tool is intended to speed up the process of collecting water well PDFs from the DNR website" ,
+      #options = {'build_exe': {'includes': add_ons}},
+      options = {
+            'build_exe': {
+                  'include_files':[
+                        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+                        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+                  ],
+                  'packages': add_ons,  
+            }
+      },
+      executables = [Executable("collect-and-capture-v2.py")]
+      )
+      #executables = [Executable("collect-and-capture-v2.py", base="Win32GUI")])
